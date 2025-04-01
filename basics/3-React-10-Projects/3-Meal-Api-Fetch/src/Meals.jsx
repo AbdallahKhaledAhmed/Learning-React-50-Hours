4dc1109dd890ea0d82dc4135ef5b5b17da81b917
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-04-01 22:13:02 +0200

    finished meals app and learned abit about axios

--- basics/3-React-10-Projects/3-Meal-Api-Fetch/src/Meals.jsx
+++ basics/3-React-10-Projects/3-Meal-Api-Fetch/src/Meals.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
 const Meals = () => {
-  return <div>Meals</div>;
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        setMeals(res.data.meals);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <section className="items-container">
+      {meals.map(({ strMeal, strMealThumb, idMeal }) => (
+        <section className="card">
+          <img src={strMealThumb} alt={strMeal} />
+          <section className="content">
+            <p>{strMeal}</p>
+            <p>#{idMeal}</p>
+          </section>
+        </section>
+      ))}
+    </section>
+  );
 };
 
 export default Meals;
