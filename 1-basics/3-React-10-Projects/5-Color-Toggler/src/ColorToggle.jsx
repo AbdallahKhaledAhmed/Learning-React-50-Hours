e9174b6c2f8cecd145bebcca4440bd1d83134ec8
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-04-02 03:40:02 +0200

    finished color toggle project

--- 1-basics/3-React-10-Projects/5-Color-Toggler/src/ColorToggle.jsx
+++ 1-basics/3-React-10-Projects/5-Color-Toggler/src/ColorToggle.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./styles.css";
 
 const ColorToggle = () => {
-  return <div>ColorToggle</div>;
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const [textColor, setTextColor] = useState("#1b1b1b");
+  return (
+    <section style={{ backgroundColor, color: textColor }}>
+      <button
+        style={{ color: textColor, border: `2px solid ${textColor}` }}
+        onClick={() => {
+          setBackgroundColor(backgroundColor === "white" ? "black" : "white");
+          setTextColor(textColor === "#1b1b1b" ? "#ffa31a" : "#1b1b1b");
+        }}
+      >
+        {backgroundColor.toUpperCase()} Mode
+      </button>
+      <section className="content">
+        <h1>
+          Welcome to the
+          <br /> real world! ...
+        </h1>
+      </section>
+    </section>
+  );
 };
 
 export default ColorToggle;
