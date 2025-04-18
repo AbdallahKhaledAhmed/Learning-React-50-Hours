0a726bade014e169a9085b6e5ce49b23999d4449
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-04-19 00:53:09 +0200

    Refactor App component to replace CounterReducer with UserList for user data display

--- 3-Typescript-With-React/src/App.tsx
+++ 3-Typescript-With-React/src/App.tsx
@@ -4,7 +4,7 @@ import User from "./components/User";
 import UserInfo from "./components/UserInfo";
 import AdminInfo from "./components/AdminInfo";
 import { myContext } from "./components/ContextAPI";
-import CounterReducer from "./components/CounterReducer";
+import UserList from "./components/UserList";
 function App() {
   const { count, increment, decrement } = useContext(myContext);
 
@@ -30,7 +30,7 @@ function App() {
       <h1>{count}</h1>
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
-      <CounterReducer />
+      <UserList />
     </>
   );
 }
--- 3-Typescript-With-React/src/components/UserList.tsx
+++ 3-Typescript-With-React/src/components/UserList.tsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+type userType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: { lat: string; lng: string };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
 const UserList = () => {
-  return <div>UserList</div>;
+  const [data, setData] = useState<userType[] | null>(null);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users", { method: "GET" }).then(
+      (res) =>
+        res.json().then((res) => {
+          setData(res);
+        })
+    );
+  }, []);
+  return (
+    <div>
+      {data !== null
+        ? data.map((i) => (
+            <p>
+              id: {i.id} <br /> name: {i.name} <br /> username: {i.username}{" "}
+              <br />
+              email: {i.email} <br /> phone: {i.phone} <br />
+            </p>
+          ))
+        : "Error"}
+    </div>
+  );
 };
 
 export default UserList;
