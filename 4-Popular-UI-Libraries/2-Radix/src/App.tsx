6f2b2a94a17ae627ecfe7d0f58f668aee1289e41
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-04-19 15:36:31 +0200

    Radix

---
+++ 4-Popular-UI-Libraries/2-Radix/src/App.tsx
@@ -0,0 +1,15 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
+    </>
+  );
+}
+
+export default App;
